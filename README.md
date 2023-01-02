446661bf7d7da3ffa4ef972392036ecbad20c5bd
Filip Hracek <filip.hracek@gmail.com>
2023-01-02 23:11:51 +0100

    Add nicer output

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+A tiny command-line newsreader.
+
+Just execute `wnews` and it will give you the three or four 
+most important news of the past few days, 
+as assembled by Wikipedia editors. (See the _In the news_ section
+of [Wikipedia homepage](https://en.wikipedia.org/wiki/Main_Page).)
+
+```terminal
+wnews
+→ Croatia adopts the euro and also joins the Schengen Area.
+  https://en.wikipedia.org/wiki/Croatia_and_the_euro
+
+→ Pope Emeritus Benedict XVI dies at the age of 95.
+  https://en.wikipedia.org/wiki/Pope_Benedict_XVI
+
+→ Brazilian footballer Pelé dies at the age of 82.
+  https://en.wikipedia.org/wiki/Pel%C3%A9
+
+→ A winter storm causes record-breaking low temperatures and leaves more than 90
+  people dead across North America.
+  https://en.wikipedia.org/wiki/December_2022_North_American_winter_storm
+```
+
+## Installation
+
+Clone this repository, go to the root directory of the project,
+and run
+
+```terminal
+dart pub global activate --source path .
+```
+
+After this, the executable in `bin/wnews.dart` is accessible
+on your `$PATH` by simply typing `wnews`.
--- bin/wnews.dart
+++ bin/wnews.dart
@@ -1,25 +1,104 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:wnews/wnews.dart';
 
 void main(List<String> arguments) async {
+  var parser = ArgParser(usageLineLength: 80);
+
+  parser.addFlag(
+    'links',
+    abbr: 'l',
+    help: 'Show links after each news item.',
+    defaultsTo: true,
+  );
+
+  parser.addFlag(
+    'help',
+    abbr: '?',
+    help: 'Show this help.',
+    negatable: false,
+  );
+
+  parser.addOption(
+    'width',
+    abbr: 'w',
+    help: 'Width of terminal, in characters.',
+    defaultsTo: '80',
+  );
+
+  var results = parser.parse(arguments);
+
+  final showLinks = results['links']! as bool;
+  final width = int.tryParse(results['width']!) ?? 80;
+
+  if (results['help']) {
+    print('Print the most important news of the past few days\n'
+        'as compiled by Wikipedia editors.\n\n'
+        'Usage:');
+    print(parser.usage);
+    exit(0);
+  }
+
   final client = HttpClient();
-  final request = await client.getUrl(Uri.parse(
-      'https://en.m.wikipedia.org/wiki/Main_Page'));
+  final request = await client
+      .getUrl(Uri.parse('https://en.m.wikipedia.org/wiki/Main_Page'));
   final response = await request.close();
   final html = await utf8.decodeStream(response);
 
   final news = extractNews(html);
 
   for (final item in news) {
-    print(item.text);
-    if (item.link != null) {
-      final url = Uri.parse('https://en.wikipedia.org/').resolveUri(item.link!);
-      // print('→ $url');
-      print('');
-    }
+    final string = _writeItem(item, width, showLinks);
+    print(string);
   }
 
   client.close(force: true);
+}
+
+String _writeItem(NewsItem item, int width, bool showLinks, {int indent = 2, String bullet = '→'}) {
+  final buf = StringBuffer();
+
+  var column = 0;
+
+  void write(String message, bool withBullet) {
+    assert(!message.contains('\n'));
+    assert(column == 0);
+    if (withBullet) {
+      buf.write(bullet);
+      buf.write(' ' * (indent - bullet.length));
+    } else {
+      buf.write(' ' * indent);
+    }
+    column += indent;
+    final words = message.split(' ');
+    var first = true;
+
+    for (final word in words) {
+      if (column + word.length > width) {
+        buf.write('\n');
+        buf.write(' ' * indent);
+        buf.write(word);
+        column = indent + word.length;
+      } else {
+        if (!first) buf.write(' ');
+        buf.write(word);
+        column += 1 + word.length;
+      }
+      first = false;
+    }
+    buf.writeln();
+    column = 0;
+  }
+
+  write(item.text, true);
+  if (item.link != null) {
+    final url = Uri.parse('https://en.wikipedia.org/').resolveUri(item.link!);
+    if (showLinks) {
+      write('$url', false);
+    }
+  }
+
+  return buf.toString();
 }

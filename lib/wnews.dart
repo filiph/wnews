d19080e694ef84a7e54d59d733cc30c4aae5ef01
Samo8 <samodubovec@gmail.com>
2023-11-02 19:20:10 +0100

    feat: small enhancements and updates

--- lib/wnews.dart
+++ lib/wnews.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
-import 'package:xpath_selector/xpath_selector.dart';
 import 'package:html/parser.dart' as html;
 
-/// Parses the JSON response from
-/// https://api.wikimedia.org/feed/v1/wikipedia/en/featured/2023/10/30
-/// where date is current day and language is parameter
+/// Parses the JSON response from URLs such as
+/// https://api.wikimedia.org/feed/v1/wikipedia/en/featured/2023/10/30.
 /// and extracts news items.
 ///
-/// For each item uses story and links parameters
+/// For each item, this method uses story and links parameters to extract [NewsItem].
+///
+/// Structure of url defined at https://api.wikimedia.org/wiki/Feed_API/Reference/Featured_content
 List<NewsItem> extractNews(String json) {
   final parsedJson = jsonDecode(json) as Map<String, dynamic>;
   final news = parsedJson['news'] as List<dynamic>?;
@@ -17,7 +17,9 @@ List<NewsItem> extractNews(String json) {
     throw StateError('The list is empty');
   }
 
-  final newsList = news.map((n) {
+  var newsList = <NewsItem>[];
+
+  for (final n in news) {
     final currentNew = n['story'];
 
     // Parse the HTML content within the "story" field
@@ -33,21 +35,10 @@ List<NewsItem> extractNews(String json) {
       final url = links.first['content_urls']['desktop']['page'] as String?;
       if (url != null) uri = Uri.tryParse(url);
     }
-
-    return NewsItem(newsContent, uri);
-  }).toList(growable: false);
+    newsList.add(NewsItem(newsContent, uri));
+  }
 
   return newsList;
-}
-
-NewsItem extract(XPathNode node) {
-  final text = node.text!.replaceAll(' (pictured)', '');
-  final linkElement = node.queryXPath('//b//a').node;
-  Uri? link;
-  if (linkElement != null) {
-    link = Uri.tryParse(linkElement.attributes['href'] ?? '');
-  }
-  return NewsItem(text, link);
 }
 
 /// This represents a single item of news as seen on Wikipedia's
